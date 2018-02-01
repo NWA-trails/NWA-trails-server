dddc8f8abf8c8aeec93bddf4e9b412f058ca7d7b
BlazingDamon <blazingdamon@gmail.com>
2018-02-01 17:07:08 -0600

    changed text

--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -1,4 +1,4 @@
-package demo;
+package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +12,7 @@ public class DemoApplication {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Hello World!";
+		return "{\"text\":\"Hello World!\"}";
 	}
 
 	public static void main(String[] args) {
