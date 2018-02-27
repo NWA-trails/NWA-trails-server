2470fe8f6e1fcc42f5b65c79fdd8f4f713a99788
BlazingDamon <blazingdamon@gmail.com>
2018-02-26 19:36:44 -0600

    added getters to UserModel

--- src/main/java/com/teamtwo/trails/user/UserModel.java
+++ src/main/java/com/teamtwo/trails/user/UserModel.java
@@ -32,4 +32,12 @@ public class UserModel {
     public void setId(long id) {
         this.id = id;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
