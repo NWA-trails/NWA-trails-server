f9b3628066b633d322f5ebaf40f7162efe745fd2
BlazingDamon <blazingdamon@gmail.com>
2018-02-26 18:50:59 -0600

    print out userModel

--- src/main/java/com/teamtwo/trails/user/UserService.java
+++ src/main/java/com/teamtwo/trails/user/UserService.java
@@ -10,6 +10,7 @@ public class UserService {
     UserRepository userRepository;
 
     public void register(UserModel userModel) {
+        System.out.println("userModel: _________________" + userModel);
         userRepository.save(userModel);
     }
 
