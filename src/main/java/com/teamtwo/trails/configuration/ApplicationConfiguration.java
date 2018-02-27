dc4d3842c4a96f6aac05fc72899ff217e4b0474c
BlazingDamon <blazingdamon@gmail.com>
2018-02-26 18:27:05 -0600

    added spring.datasource to property files

---
+++ src/main/java/com/teamtwo/trails/configuration/ApplicationConfiguration.java
@@ -0,0 +1,13 @@
+package com.teamtwo.trails.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class ApplicationConfiguration {
+
+
+}
