358a26ee547282d96079ab16f64bd4774346d442
Adrian Adams <adrianadams@Adrians-MacBook-Pro.local>
2018-04-05 14:58:45 -0600

    Account information endpoints

---
+++ src/main/java/com/teamtwo/trails/accountInformation/AccountInformationService.java
@@ -0,0 +1,19 @@
+package com.teamtwo.trails.accountInfromation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountInformationService {
+
+    @Autowired
+    AccountInformationRepository accountInformationRepository;
+
+    public AccountInformationModel getAccountInformation(String userName) {
+        return accountInformationRepository.findByUsername(userName);
+    }
+
+    public void updateAccountInformation(AccountInformationModel accountInformationModel) {
+        accountInformationRepository.save(accountInformationModel);
+    }
+}
