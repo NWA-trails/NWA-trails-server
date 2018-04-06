2cbb75e1f5af3e489cfea4e086fd34f21785f741
BlazingDamon <blazingdamon@gmail.com>
2018-04-05 20:06:58 -0500

    fixed url on POI controller

--- src/main/java/com/teamtwo/trails/pointOfInterest/PointOfInterestController.java
+++ src/main/java/com/teamtwo/trails/pointOfInterest/PointOfInterestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/trailcondition")
+@RequestMapping("/pointofinterest")
 public class PointOfInterestController {
 
     @Autowired
