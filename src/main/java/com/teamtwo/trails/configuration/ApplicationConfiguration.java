a2e29d852190d3f1c037f9641c439abb1692e976
adrianadams <ama021@uark.edu>
2018-04-13 11:50:35 -0500

    Revert "Improved security"
    This reverts commit ab80e59

--- src/main/java/com/teamtwo/trails/configuration/ApplicationConfiguration.java
+++ src/main/java/com/teamtwo/trails/configuration/ApplicationConfiguration.java
@@ -1,35 +1,21 @@
 package com.teamtwo.trails.configuration;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.xml.ws.WebEndpoint;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
 public class ApplicationConfiguration {
 
-    private String secretKey;
-    private long tokenValidityInSeconds;
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    @Qualifier("secret_key")
-    public String getSecretKey() {
-        return "random_key_hello";
-    }
 
-    @Bean
-    @Qualifier("token_validity")
-    public long getTokenValidityInSeconds() {
-        return 2592000;
-    }
+
 }
--- src/main/java/com/teamtwo/trails/user/UserController.java
+++ src/main/java/com/teamtwo/trails/user/UserController.java
@@ -1,18 +1,14 @@
 package com.teamtwo.trails.user;
 
-import com.teamtwo.trails.Security.JWT.TokenProvider;
 import com.teamtwo.trails.wrapper.UpdatePasswordWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.xml.ws.Response;
 import java.util.List;
 
 @RestController
@@ -20,20 +16,7 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    PasswordEncoder passwordEncoder;
-
-    private final UserService userService;
-    private final TokenProvider tokenProvider;
-    private final AuthenticationManager authenticationManager;
-
-    public UserController(UserService userService,
-                          TokenProvider tokenProvider,
-                          AuthenticationManager authenticationManager) {
-        this.userService = userService;
-        this.tokenProvider = tokenProvider;
-        this.authenticationManager = authenticationManager;
-    }
-
+    UserService userService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     String home() {
@@ -45,40 +28,18 @@ public class UserController {
         return new ResponseEntity<>(userService.getAll(),HttpStatus.OK);
     }
 
-
-    @RequestMapping(value = "/authenticate", method = RequestMethod.GET)
-    public boolean authenticate() {
-        /*
-            This will be called at the startup of the app to check if the
-            token is still valid. Will return true if still valid
-         */
-
-        return true;
+    @RequestMapping(value = "/register", method= RequestMethod.POST)
+    public ResponseEntity<String> register( @RequestBody UserModel userModel ) {
+        userService.register(userModel);
+        return new ResponseEntity<>("{\"message\":\"Created user "+userModel.getUsername()+" successfully.\"}", HttpStatus.OK);
     }
 
-
-    @RequestMapping(value = "/login", method= RequestMethod.POST)
-    public ResponseEntity<String> login(@RequestBody UserModel userModel) {
-        System.out.println("Logging in user: " + userModel.getUsername() + " with password: " + userModel.getPassword());
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userModel.getUsername(), passwordEncoder.encode(userModel.getPassword()));
-
-        try {
-            //this.authenticationManager.authenticate(authenticationToken);
-            System.out.println("Successfully logged in user: " + userModel.getUsername());
-            return new ResponseEntity<>(this.tokenProvider.createToken(userModel.getUsername()), HttpStatus.OK);
-        } catch (AuthenticationException e) {
-            System.out.println("Failed to login in user with error: " + e.getMessage());
-            return new ResponseEntity<>("{\"message\":\"Username or Password is invalid\"}", HttpStatus.UNAUTHORIZED);
-        }
-    }
-
-    @RequestMapping(value = "/signup", method= RequestMethod.POST)
-    public ResponseEntity<String> signup( @RequestBody UserModel userModel ) {
-        if (userService.usernameExists(userModel.getUsername()) == true) {
-            return new ResponseEntity<>("{\"message\":\"Username already exists.\"}", HttpStatus.CONFLICT);
+    @RequestMapping(value = "/validate", method= RequestMethod.POST)
+    public ResponseEntity<String> validate(@RequestBody UserModel userModel ) {
+        if (userService.isPassword(userModel.getPassword(), userModel.getUsername())) {
+            return new ResponseEntity<>("{\"message\":\"Login successful.\"}", HttpStatus.OK);
         } else {
-            userService.register(userModel);
-            return new ResponseEntity<>(this.tokenProvider.createToken(userModel.getUsername()), HttpStatus.OK);
+            return new ResponseEntity<>("{\"message\":\"Login failed.\"}", HttpStatus.UNAUTHORIZED);
         }
     }
 
