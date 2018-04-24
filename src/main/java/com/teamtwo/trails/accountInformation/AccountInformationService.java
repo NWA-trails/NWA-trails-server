aebf87c4b5cb87de5b9e57e086fff30e2cb8a7c0
ama021 <ama021@uark.edu>
2018-04-23 23:48:42 -0500

    Debuggin

--- src/main/java/com/teamtwo/trails/accountInformation/AccountInformationService.java
+++ src/main/java/com/teamtwo/trails/accountInformation/AccountInformationService.java
@@ -16,6 +16,8 @@ public class AccountInformationService {
     public void updateAccountInformation(AccountInformationModel accountInformationModel) {
         AccountInformationModel accountInfo = this.accountInformationRepository.findByUsername(accountInformationModel.getUsername());
 
+        System.out.println("Account Model ID is: " + accountInformationModel.getId() + " username is: " + accountInformationModel.getUsername());
+        System.out.println("Account Info ID is: " + accountInfo.getId() + " username is: " + accountInfo.getUsername());
         if (accountInfo.getUsername() != null) {
             accountInfo.setDateofbirth(accountInformationModel.getDateofbirth());
             accountInfo.setHeight(accountInformationModel.getHeight());
