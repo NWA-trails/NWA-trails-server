4c8eebe7fa8db007f4c807d68d6983c3522b7ea9
ama021 <ama021@uark.edu>
2018-04-24 20:22:40 -0500

    I dont know what it wants, so picky

--- src/main/java/com/teamtwo/trails/user/UserRepository.java
+++ src/main/java/com/teamtwo/trails/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.teamtwo.trails.user;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +11,7 @@ public interface UserRepository extends CrudRepository<UserModel, Long> {
     List<UserModel> findByUsername(String username);
     List<UserModel> findAll();
 
-    @Query("Select USERINFO.username, USERINFO.first_name, USERINFO.last_name, USERINFO.email, USERINFO.role, ACCOUNT.dateofbirth, ACCOUNT.height, ACCOUNT.weight FROM \"user\" as USERINFO LEFT JOIN account_information as ACCOUNT ON USERINFO.username = ACCOUNT.username WHERE USERINFO.username = $1;")
+    @Modifying
+    @Query("Select USERINFO.username, USERINFO.first_name, USERINFO.last_name, USERINFO.email, USERINFO.role, ACCOUNT.dateofbirth, ACCOUNT.height, ACCOUNT.weight FROM \"user\" as USERINFO LEFT JOIN account_information as ACCOUNT ON USERINFO.username = ACCOUNT.username WHERE USERINFO.username = :username;")
     public UserDetailsDTO getUserDetails(@Param("username") String username);
 }
