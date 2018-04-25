9210989aac73a4bec84e619a9544b26f4b5d4e95
ama021 <ama021@uark.edu>
2018-04-24 20:03:05 -0500

    Guess it didn't like me injecting the query into @query

--- src/main/java/com/teamtwo/trails/user/UserRepository.java
+++ src/main/java/com/teamtwo/trails/user/UserRepository.java
@@ -7,23 +7,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<UserModel, Long> {
-    String userDetailsQuery = "Select " +
-            "USERINFO.username, " +
-            "USERINFO.first_name, " +
-            "USERINFO.last_name, " +
-            "USERINFO.email, " +
-            "USERINFO.role, " +
-            "ACCOUNT.dateofbirth, " +
-            "ACCOUNT.height, " +
-            "ACCOUNT.weight " +
-            "FROM \"user\" as USERINFO " +
-            "LEFT JOIN account_information as ACCOUNT " +
-            "ON USERINFO.username = ACCOUNT.username " +
-            "WHERE USERINFO.username = :username";
-
     List<UserModel> findByUsername(String username);
     List<UserModel> findAll();
 
-    @Query(userDetailsQuery)
+    @Query("Select USERINFO.username, USERINFO.first_name, USERINFO.last_name, USERINFO.email, USERINFO.role, ACCOUNT.dateofbirth, ACCOUNT.height, ACCOUNT.weight FROM \"user\" as USERINFO LEFT JOIN account_information as ACCOUNT ON USERINFO.username = ACCOUNT.username WHERE USERINFO.username = :username;")
     public UserDetailsDTO getUserDetails(@Param("username") String username);
 }
