cdfe8927e5a3ec50ef294c4d9888ebd69cefd315
ama021 <ama021@uark.edu>
2018-04-25 02:20:03 -0500

    Reverting security...

--- src/main/java/com/teamtwo/trails/user/UserController.java
+++ src/main/java/com/teamtwo/trails/user/UserController.java
@@ -1,14 +1,10 @@
 package com.teamtwo.trails.user;
 
-import com.teamtwo.trails.Security.JWT.TokenProvider;
+
 import com.teamtwo.trails.wrapper.UpdatePasswordWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
