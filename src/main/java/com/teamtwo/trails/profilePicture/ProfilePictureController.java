9e3fe8c6cce95f8d3395a1d03be677757f9671c1
adrianadams <ama021@uark.edu>
2018-04-25 20:55:15 -0500

    Profile Picture update

--- src/main/java/com/teamtwo/trails/profilePicture/ProfilePictureController.java
+++ src/main/java/com/teamtwo/trails/profilePicture/ProfilePictureController.java
@@ -3,10 +3,7 @@ package com.teamtwo.trails.profilePicture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,14 +14,8 @@ public class ProfilePictureController {
     @Autowired
     ProfilePictureService profilePictureService;
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public ResponseEntity<String> add(@RequestBody ProfilePictureModel profilePictureModel) {
-        profilePictureService.add(profilePictureModel);
-        return new ResponseEntity<>("{\"message\":\"Created profile picture.\"}", HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/getByUsername", method = RequestMethod.GET)
-    public ResponseEntity<List<ProfilePictureModel>> getByUsername(String username) {
+    @RequestMapping(value = "/getProfilePicture/{username}", method = RequestMethod.GET)
+    public ResponseEntity<String> getByUsername(@PathVariable String username) {
         return new ResponseEntity<>(profilePictureService.getByUsername(username), HttpStatus.OK);
     }
 
--- src/main/java/com/teamtwo/trails/profilePicture/ProfilePictureService.java
+++ src/main/java/com/teamtwo/trails/profilePicture/ProfilePictureService.java
@@ -11,24 +11,25 @@ public class ProfilePictureService {
     @Autowired
     ProfilePictureRepository profilePictureRepository;
 
-    public List<ProfilePictureModel> getByUsername(String username) {
-        return profilePictureRepository.findByUsername(username);
-    }
-
-    public void add(ProfilePictureModel profilePictureModel) {
-        profilePictureRepository.save(profilePictureModel);
+    public String getByUsername(String username) {
+        return new String(profilePictureRepository.findByUsername(username).get(0).getImage());
     }
 
     public boolean update(ProfilePictureModel profilePictureModel) {
         List<ProfilePictureModel> oldProfilePictureModels = profilePictureRepository.findByUsername(profilePictureModel.getUsername());
         ProfilePictureModel profilePicture;
-        if (oldProfilePictureModels.get(0) != null) {
-            profilePicture = oldProfilePictureModels.get(0);
-            profilePicture.setImage(profilePictureModel.getImage());
-            profilePictureRepository.save(profilePicture);
-            return true;
-        } else {
+        try {
+            if (oldProfilePictureModels.get(0) != null) {
+                profilePicture = oldProfilePictureModels.get(0);
+                profilePicture.setImage(profilePictureModel.getImage());
+                profilePictureRepository.save(profilePicture);
+            } else {
+                profilePictureRepository.save(profilePictureModel);
+            }
+        } catch(RuntimeException e) {
             return false;
         }
+
+        return true;
     }
 }
